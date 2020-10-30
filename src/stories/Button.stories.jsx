6d8d0c12c6fa7acb67f52ba19c4668cfd0db06bd
Lucas <lucas.pimenta94@outlook.com>
2020-10-30 00:17:33 -0300

    remove eslint ignore rule comment

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -10,7 +10,6 @@ export default {
   },
 };
 
-// eslint-disable-next-line react/jsx-props-no-spreading
 const Template = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
