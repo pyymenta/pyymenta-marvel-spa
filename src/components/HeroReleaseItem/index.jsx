47b3f6945f85e8fc8d72387f8be952c3e3abe015
Lucas <lucas.pimenta94@outlook.com>
2020-11-01 20:40:39 -0300

    Add HeroReleaseItem component

---
+++ src/components/HeroReleaseItem/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './index.css';
+
+const HeroItem = ({ heroImage, heroName }) => {
+  return (
+    <div className='hero--release-item'>
+      <img className='hero--release-item__image' src={heroImage} alt={heroName} />
+      <div className='hero--release-item__info-wrapper'>
+        <span className='hero--release-item__name'>${heroName}</span>
+      </div>
+    </div>
+  );
+};
+
+HeroItem.defaultProps = {
+  heroName: '',
+  heroImage: '',
+};
+
+HeroItem.propTypes = {
+  heroName: PropTypes.string,
+  heroImage: PropTypes.string,
+};
+
+export default HeroItem;
---
+++ src/stories/HeroReleaseItem.stories.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import HeroReleaseItem from '../components/HeroReleaseItem';
+import '../styles/index.css';
+
+export default {
+  title: 'Example/HeroReleaseItem',
+  component: HeroReleaseItem,
+};
+
+const divStyle = {
+  width: '90%',
+};
+
+const Template = (props) => (
+  <div style={divStyle}>
+    <HeroReleaseItem {...props} />
+  </div>
+);
+
+export const Hero = Template.bind({});
+Hero.args = {
+  heroName: '3-D Man',
+  heroImage: 'http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg',
+};
