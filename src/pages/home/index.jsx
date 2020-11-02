2bb278ad0d4875183cefd6ea5cf9c05a0161e7ea
Lucas <lucas.pimenta94@outlook.com>
2020-11-02 18:01:00 -0300

    update home to consume getCharacters services

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.css';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
@@ -7,86 +7,66 @@ import HeroCounter from '../../components/HeroCounter';
 import OrderAction from '../../components/OrderAction';
 import Toggle from '../../components/Toggle';
 import HeroItem from '../../components/HeroItem';
+import getCharacters from '../../services/getCharacters';
 
-const Home = () => (
-  <main className='home' role='main'>
-    <Header>
-      <h1>Explore o Universo</h1>
-      <h2>
-        Mergulhe no domínio deslumbrante de todos os personagens clássicos que você
-        ama - e aqueles que você descobrirá em breve!
-      </h2>
-    </Header>
+const Home = () => {
+  const [heroes, setHeroes] = useState([]);
+  const [heroesCount, setHeroesCount] = useState(0);
 
-    <div className='searchbar-wrapper'>
-      <SearchInput searchType='brand' />
-    </div>
+  useEffect(() => {
+    getCharacters()
+      .then((res) => {
+        const characters = res.data.results.map((character) => ({
+          heroId: character.id,
+          heroName: character.name,
+          heroImage: `${character.thumbnail.path}.${character.thumbnail.extension}`,
+          isFavorite: false,
+        }));
 
-    <div className='actions-wrapper'>
-      <div className='hero-count-wrapper'>
-        <HeroCounter heroCount={6} />
+        setHeroesCount(res.data.count);
+        setHeroes(characters);
+      })
+      .catch(() => []);
+  }, []);
+
+  return (
+    <main className='home' role='main'>
+      <Header>
+        <h1>Explore o Universo</h1>
+        <h2>
+          Mergulhe no domínio deslumbrante de todos os personagens clássicos que você
+          ama - e aqueles que você descobrirá em breve!
+        </h2>
+      </Header>
+
+      <div className='searchbar-wrapper'>
+        <SearchInput searchType='brand' />
       </div>
-      <div className='order-action-wrapper'>
-        <OrderAction type='hero' text='Ordenar por nome - A/Z' />
-        <div className='starred-wrapper'>
-          <Toggle />
-          <OrderAction type='heart' text='Somente favoritos' />
+
+      <div className='actions-wrapper'>
+        <div className='hero-count-wrapper'>
+          <HeroCounter heroCount={heroesCount} />
+        </div>
+        <div className='order-action-wrapper'>
+          <OrderAction type='hero' text='Ordenar por nome - A/Z' />
+          <div className='starred-wrapper'>
+            <Toggle />
+            <OrderAction type='heart' text='Somente favoritos' />
+          </div>
         </div>
       </div>
-    </div>
 
-    <section className='heroes-wrapper'>
-      <div className='hero-item-wrapper'>
-        <HeroItem
-          heroName='3-D Man'
-          heroImage='http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg'
-          isFavorite={false}
-        />
-      </div>
+      <section className='heroes-wrapper'>
+        {heroes.map((hero) => (
+          <div className='hero-item-wrapper'>
+            <HeroItem {...hero} />
+          </div>
+        ))}
+      </section>
 
-      <div className='hero-item-wrapper'>
-        <HeroItem
-          heroName='3-D Man'
-          heroImage='http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg'
-          isFavorite={false}
-        />
-      </div>
-
-      <div className='hero-item-wrapper'>
-        <HeroItem
-          heroName='3-D Man'
-          heroImage='http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg'
-          isFavorite={false}
-        />
-      </div>
-
-      <div className='hero-item-wrapper'>
-        <HeroItem
-          heroName='3-D Man'
-          heroImage='http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg'
-          isFavorite={false}
-        />
-      </div>
-
-      <div className='hero-item-wrapper'>
-        <HeroItem
-          heroName='3-D Man'
-          heroImage='http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg'
-          isFavorite={false}
-        />
-      </div>
-
-      <div className='hero-item-wrapper'>
-        <HeroItem
-          heroName='3-D Man'
-          heroImage='http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg'
-          isFavorite={false}
-        />
-      </div>
-    </section>
-
-    <Footer />
-  </main>
-);
+      <Footer />
+    </main>
+  );
+};
 
 export default Home;
