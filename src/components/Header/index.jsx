037f44cb6a2c88edbfe497df417a55c052161f88
Lucas <lucas.pimenta94@outlook.com>
2020-11-01 22:46:12 -0300

    add classlist in header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,19 +3,23 @@ import PropTypes from 'prop-types';
 import './index.css';
 import logo from './logo.svg';
 
-const Header = ({ children }) => (
-  <header>
-    <img src={logo} alt='Marvel Logo' />
+const Header = ({ classList, children, logoImageWidth }) => (
+  <header className={classList}>
+    <img width={logoImageWidth} src={logo} alt='Marvel Logo' />
     {children}
   </header>
 );
 
 Header.defaultProps = {
   children: React.createElement('div'),
+  logoImageWidth: 500,
+  classList: '',
 };
 
 Header.propTypes = {
   children: PropTypes.node,
+  logoImageWidth: PropTypes.number,
+  classList: PropTypes.string,
 };
 
 export default Header;
