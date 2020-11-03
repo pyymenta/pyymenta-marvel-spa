e24ec8c4bfd2d132f0ca85768a1c69a1976eb05d
Lucas <lucas.pimenta94@outlook.com>
2020-11-02 22:47:38 -0300

    update release item style

--- src/components/HeroReleaseItem/index.jsx
+++ src/components/HeroReleaseItem/index.jsx
@@ -4,10 +4,10 @@ import './index.css';
 
 const HeroItem = ({ heroImage, heroName }) => {
   return (
-    <div className='hero--release-item'>
-      <img className='hero--release-item__image' src={heroImage} alt={heroName} />
-      <div className='hero--release-item__info-wrapper'>
-        <span className='hero--release-item__name'>{heroName}</span>
+    <div className='hero-release-item'>
+      <img className='hero-release-item__image' src={heroImage} alt={heroName} />
+      <div className='hero-release-item__info-wrapper'>
+        <span className='hero-release-item__name'>{heroName}</span>
       </div>
     </div>
   );
