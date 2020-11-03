19f5c1b69716c10bfcaafcd1d305819b6baf18a5
Lucas <lucas.pimenta94@outlook.com>
2020-11-02 22:52:21 -0300

    update hero page with api infos and remove footer

--- src/pages/hero/index.jsx
+++ src/pages/hero/index.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import { getCharacterById } from '../../services/characters';
+import getComicsByCharacterId from '../../services/comics';
 import Header from '../../components/Header';
-import Footer from '../../components/Footer';
 import SearchInput from '../../components/SearchInput';
 import HeroDetails from '../../components/HeroDetails';
 import HeroReleaseItem from '../../components/HeroReleaseItem';
@@ -9,6 +10,44 @@ import './index.css';
 
 const Hero = () => {
   const history = useHistory();
+  const { id } = useParams();
+  const [heroData, setHeroData] = useState({});
+  const [heroComics, setHeroComics] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async (characterId) => {
+      const heroResult = await getCharacterById(characterId);
+      const comicsResult = await getComicsByCharacterId(characterId);
+      const hero =
+        heroResult.data.results.map((character) => ({
+          heroId: character.id,
+          heroName: character.name,
+          heroDescription: character.description,
+          comicsBooks: character.comics.available,
+          comics: character.comics,
+          movies: character.series.available,
+          ratingValue: 5,
+          heroImage: `${character.thumbnail.path}.${character.thumbnail.extension}`,
+          lastComicDate: character.modified,
+          isFavorite: false,
+        }))[0] || {};
+      const comics = comicsResult.data.results.map((comic) => ({
+        heroName: comic.title,
+        heroImage: `${comic.thumbnail.path}.${comic.thumbnail.extension}`,
+        modifiedDate: new Date(
+          comic.dates.find((date) => date.type === 'onsaleDate').date
+        ).toString(),
+      }));
+
+      setHeroComics(comics);
+      setHeroData({
+        ...hero,
+        lastComicDate: comics[comics.length - 1].modifiedDate,
+      });
+    };
+
+    fetchData(id);
+  }, []);
 
   const redirectToHome = () => {
     if (!history) {
@@ -19,16 +58,19 @@ const Hero = () => {
   };
 
   return (
-    <main className='hero-details-page' role='main'>
+    <main className={`hero-details-page ${id}`} role='main'>
       <Header classList='hero-details-header' logoImageWidth={200}>
         <SearchInput onSearchInput={redirectToHome} />
       </Header>
-      <HeroDetails heroName='Huck' />
+      <HeroDetails {...heroData} />
       <div className='last-releases'>
         <span className='last-releases__title'>Últimos lançamentos</span>
-        <HeroReleaseItem />
+        <div className='releases-wrapper'>
+          {heroComics.map((heroComic) => (
+            <HeroReleaseItem {...heroComic} />
+          ))}
+        </div>
       </div>
-      <Footer />
     </main>
   );
 };
