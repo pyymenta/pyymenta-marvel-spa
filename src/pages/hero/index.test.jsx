6f9d2b5d13ebc5ff5543e11ed3e751e9ae2b8342
Lucas <lucas.pimenta94@outlook.com>
2020-11-04 17:43:30 -0300

    remove some ununsed specs

--- src/pages/hero/index.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Hero from './index';
-
-test('renders home text', () => {
-  render(<Hero />);
-  const linkElement = screen.getAllByText(/Hero Details/i)[0];
-  expect(linkElement).toBeInTheDocument();
-});
--- src/pages/home/index.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Home from './index';
-
-test('renders home text', () => {
-  render(<Home />);
-  const linkElement = screen.getAllByText(/Home/i)[0];
-  expect(linkElement).toBeInTheDocument();
-});
